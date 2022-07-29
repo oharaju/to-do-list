965af362121e8dbcb3be437f7accc28e65ab96a7
Júlia Ohara <juliaohara.br@gmail.com>
2022-07-28 22:40:34 -0300

    feat: add button function to edit

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function generateList() {
 
     const btnRemove = document.createElement('button');
     const textRemove = document.createTextNode('delete');
+    btnRemove.setAttribute("class", "removerItem");
     btnRemove.appendChild(textRemove);
 
     btnRemove.addEventListener("click", function() {
@@ -28,6 +29,7 @@ function generateList() {
       updateLocalStorage();
       removeList();
       generateList();
+      updateTaskArray();
     });
 
     const btnUpdate = document.createElement('button');
@@ -39,15 +41,6 @@ function generateList() {
     inputUpdate.setAttribute("class", "inputEdit");
     inputUpdate.appendChild(btnUpdate);
 
-    btnUpdate.addEventListener("click", function() {
-      const position = tasks.indexOf(task);
-
-      updateTaskArray(position);
-      updateLocalStorage();
-      removeList();
-      generateList();
-    });
-
     item.appendChild(document.createTextNode(task));
     item.appendChild(btnUpdate);
     item.appendChild(btnRemove);
@@ -55,12 +48,24 @@ function generateList() {
   })
 }
 
-function updateTaskArray(event) {
-  const btnUpdate = event.target;
-  const item = btnUpdate.closest('li');
-  btnUpdate.classList.add('updating')
+function updateTaskArray() {
+  const buttonsUpdate = document.querySelectorAll(".updateItem");
 
-  item.classList.add('updating')
+  for (let i = 0; i <  buttonsUpdate.length; i++) {
+    const buttonUpdateItem =  buttonsUpdate[i];
+
+    buttonUpdateItem.addEventListener("click", function (event){
+      const buttonUpdate = event.target;
+      // const closestUpdate = buttonUpdate.closest('li');
+      // closestUpdate.classList.add('updating');
+
+      console.log(buttonUpdate)
+    });
+  }
+}
+
+function removeTaskArray(position) {
+  tasks.splice(position, 1);
 }
 
 function clearInput() {
@@ -71,9 +76,6 @@ function removeList() {
   list.innerHTML = "";
 }
 
-function removeTaskArray(position) {
-  tasks.splice(position, 1);
-}
 
 function validateInput(value) {
   return value.length > 0
@@ -81,6 +83,7 @@ function validateInput(value) {
 
 function handleSubmit(e) {
   e.preventDefault();
+
   const valueInput = taskInput.value;
 
   if(validateInput(valueInput)) {
